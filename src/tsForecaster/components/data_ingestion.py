9d9343b3efdfcb93683e27296ab0ff06a3d992fb
Bhuvanesh Prasad <pbhuvanesh3@gmail.com>
2024-07-13 11:31:42 +0530

    updated data ingestion with yfinance

--- src/tsForecaster/components/data_ingestion.py
+++ src/tsForecaster/components/data_ingestion.py
@@ -1,5 +1,7 @@
 import os
 import opendatasets as od
+import yfinance as yf
+import pandas as pd
 from tsForecaster.entity.config_entity import DataIngestionConfig
 from tsForecaster import logger
 
@@ -17,3 +19,26 @@ class DataIngestion:
             logger.info(f"Downloaded data from {dataset_url} into file {download_dir}")
         except Exception as e:
             raise e
+    
+    def update_file(self) ->None:
+        try:
+            download_dir = self.config.data_dir
+            ticker = yf.Ticker("^NSEI")
+            history = ticker.history(start='2024-07-08', interval='1d')
+            history.drop(columns=['Volume', 'Dividends', 'Stock Splits'], inplace=True)
+            history = round(history, 2)
+            history['Index Name'] = "NIFTY 50"
+            history.reset_index(inplace=True)
+            history['Date'] = history['Date'].dt.strftime('%d %b %Y')
+            history = history[['Index Name', 'Date', 'Open', 'High', 'Low', 'Close']]
+            history = history.sort_values(by=['Date'], ascending=False)
+            df = pd.read_csv(os.path.join(download_dir ,"NIFTY 50_Historical.csv"))
+            df = pd.concat([history, df], ignore_index=True)
+            df = df[['Index Name', 'Date', 'Open', 'High', 'Low', 'Close']]
+            df['Date'] = pd.to_datetime(df['Date'])
+            df = df.drop_duplicates(subset=['Date'])
+            df.to_csv(os.path.join(download_dir ,"NIFTY 50_Historical.csv"))
+        
+        except Exception as e:
+            raise e
+    
--- src/tsForecaster/pipeline/stage_01_data_ingestion.py
+++ src/tsForecaster/pipeline/stage_01_data_ingestion.py
@@ -14,6 +14,7 @@ class DataIngestionPipeline:
             data_ingestion_config = config.get_data_ingestion_config()
             data_ingestion = DataIngestion(config=data_ingestion_config)
             data_ingestion.download_file()
+            data_ingestion.update_file()
         except Exception as e:
             raise e
 
